1a5f5c2be808ad41217f0f2eb90d87bf044fd401
TKR <tolg@cybertkr.com>
2023-04-04 02:32:02 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,11 @@
+from tool import *
+
+
+# Server tarafı
+
+server = Server("localhost", 5000)
+server.start()
+
+# Client tarafı
+client = Client("localhost", 5000)
+client.send_message("Merhaba, bu bir mesajdır.")
